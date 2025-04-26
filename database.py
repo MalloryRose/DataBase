10479da08d339773930b8428727297e97c53ac1a
MalloryRose <malloryrose1432@gmail.com>
2025-04-26 15:01:25 -0400

    Functional code

--- database.py
+++ database.py
@@ -1,44 +1,88 @@
 class database:
 
     def __init__(self):
-        pass
+        self.main_database = dict() # main dictionary
+        self.changes = dict() # dictionary to store the current changes in the current transaction
+        self.size = 0
+        self.transaction = False # track if a transaction is going on
+        
+        
 
-    def begin_transaction():
+    def begin_transaction(self):
         # starts a new transaction
-        pass
+        # at a time, only one transaction can happen
+        
+        if self.transaction:
+            raise Exception("Transaction already in progress")
+        
+        self.transaction = True
+        self.changes.clear()
+        print("Transaction started")
 
-    def put(key, value):
+    def put(self, key, value):
         # keys are string, values are integers
         # if a key doesn't exist, create a new key with the provided value 
         # otherwise update the value of an existing key
-        pass
+        
+        if self.transaction:
+            self.changes[key] = value 
+        else:
+            raise Exception("No transaction in progress")
+            
+        
+        
 
-    def get(key):
+    def get(self, key):
         # return the value associated with the key 
         # or null if doesn't exist
-        
-        pass
 
-    def commit():
+        #only return the committed stuff, in the main database
+        if key in self.main_database:
+            return self.main_database[key]
+        else:
+            print("Error: No key exists in the database")
+            return None
+            
+        
+
+    def commit(self):
         # applies changes made within the transaction too the main state
         # allowing any future gets() to "see" the changes made within the transaction
-        pass
+        
+        # merge the changes dictionary to the main_database dictionary
+        
+        if self.transaction:
+            # check that there are changes
+            if self.changes:
+                for key, value in self.changes.items():
+                  
+                        self.main_database[key] = value
+                
+            
+        
+            #end the transaction
+            self.transaction = False
+            self.changes.clear()
+            
+            print("Changes have been committed to the main database")
+        else:
+            raise Exception("No transaction in progress")
+            
+        
 
-    def rollback():
+    def rollback(self):
         # should aborts all changes made within the transaction and everything
         # should go back to the way it was before
-        pass 
+        if self.transaction:
+            self.changes.clear() 
+            self.transaction = False
+        else:
+              raise Exception("No transaction in progress")
 
 
-# if put is called when a transaction is not in progress, throw exception
-
-# get(key) can be called anytime even when a transaction is not in progress
-
-# only a single transaction may exist at a time
-
-# Within a transaction you can make as many changes to as many keys as you like.
-#However, they should not be “visible” to get(), until the transaction is committed
 
 newDatabase = database()
+
+
 
 
